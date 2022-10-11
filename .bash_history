b0a8ecb3f5cd4a83d170453c2504ce1976043488
root <root@LAPTOP-MFAF0S1U.localdomain>
2022-10-11 21:19:05 +0700

    pert4

---
+++ .bash_history
@@ -0,0 +1,3 @@
+apt update -y
+apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
---
+++ pert3/tugas.sesi4.py
@@ -0,0 +1,11 @@
+#Contoh Operator Aritmatika
+a="operator aritmatika"
+d="operator aritmatika digunakan memecahkan kasus yang berhubungan dengan hitunh menghitung"
+
+print(a)
+print(d)
+
+y, z = 20, 5
+
+penjumlahan = y+z #nilai1 ditambah nilai2
+print("Hasil dari y+z =", penjumlahan)
